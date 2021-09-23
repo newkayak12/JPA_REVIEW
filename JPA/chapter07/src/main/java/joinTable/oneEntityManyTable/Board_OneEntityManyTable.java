cc15b21c96e1f35aa5dc4f992a8124ffeb8e2184
newkayak12 <newkayak12@gmail.com>
2021-09-23 17:29:35 +0900

    oneEntityManyTable

--- JPA/chapter07/src/main/java/joinTable/oneEntityManyTable/Board_OneEntityManyTable.java
+++ JPA/chapter07/src/main/java/joinTable/oneEntityManyTable/Board_OneEntityManyTable.java
@@ -8,7 +8,9 @@ import javax.persistence.*;
 /*
     Table을 이용해서 board 테이블을 연결했다.
 
-    @SecondaryTable을 이용해서 하나의 테이블 추가적으로 연결했다. 
+    @SecondaryTable을 이용해서 하나의 테이블 추가적으로 연결했다.
+        - name : 매핑할 다른 테이블의 이름
+        - pkJoinColumns : 매핑할 다른 테이블의 기본 키 컬럼 속성
  */
 public class Board_OneEntityManyTable {
     @Id
@@ -18,6 +20,9 @@ public class Board_OneEntityManyTable {
     private String title;
 
     @Column(table = "BOARD_DETAIL_ONEENTITYMANYTABLE")
+    /*
+        컬럼을 이용해서 board_detail테이블의 컬럼에 매핑했다. 테이블을 지정하지 않으면 board로 빨려 들어간다.
+     */
     private String content;
 
 }
