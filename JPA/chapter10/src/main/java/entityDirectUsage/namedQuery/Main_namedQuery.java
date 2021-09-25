4db1dbcd35e82fd0048a6d3216dc7daca9385efd
newkayak12 <newkayak12@gmail.com>
2021-09-25 18:47:54 +0900

    JPQL

---
+++ JPA/chapter10/src/main/java/entityDirectUsage/namedQuery/Main_namedQuery.java
@@ -0,0 +1,30 @@
+package entityDirectUsage.namedQuery;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class Main_namedQuery {
+    static EntityManagerFactory emf = Persistence.createEntityManagerFactory("hibernate");
+    static EntityManager em = emf.createEntityManager();
+    static EntityTransaction tx = em.getTransaction();
+
+    public static void main(String[] args) {
+    namedQuery();
+
+    }
+
+    public static void namedQuery(){
+        tx.begin();
+        Friend_namedQuery f1 = Friend_namedQuery.builder().name("yj").birthDay("1996-06-26").phoneNumber("4088-8200").build();
+        em.persist(f1);
+        tx.commit();
+        Friend_namedQuery friend_namedQuery = em.createNamedQuery("Friend.findByFriendName",Friend_namedQuery.class).setParameter("FriendName", "yj").getSingleResult();
+        System.out.println(friend_namedQuery);
+
+        Query query = em.createNamedQuery("Friend.count");
+        Object o = query.getSingleResult();
+        System.out.println("named Query : " +o);
+// 어노테이션과 XML 중 XML이 우선권을 가진다.
+
+    }
+}
