1ff3b879ed0944f1c10263b8766a3b2deb44234f
newkayak12 <newkayak12@gmail.com>
2021-09-29 20:00:51 +0900

    JUNIT TEST

--- JPA/chapter11_web/src/test/java/com/jpa/chapter11_web/memberTest/MemberServiceTest.java
+++ JPA/chapter11_web/src/test/java/com/jpa/chapter11_web/memberTest/MemberServiceTest.java
@@ -3,22 +3,33 @@ package com.jpa.chapter11_web.memberTest;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
 import com.jpa.chapter11_web.Repository.MemberRepositoryimpl;
 import com.jpa.chapter11_web.Service.MemberServiceimpl;
 import com.jpa.chapter11_web.domain.Member;
 
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-// @Transactional
+// @RunWith(SpringRunner.class)
+@Transactional
 @TestPropertySource("classpath:application.properties")
+// @ExtendWith(SpringExtension.class)
+@RunWith(SpringRunner.class)
+@DataJpaTest
 public class MemberServiceTest {
-	@MockBean
+	@Mock
 	MemberServiceimpl service;
 	@MockBean
 	MemberRepositoryimpl repo;
@@ -27,13 +38,15 @@ public class MemberServiceTest {
 	public void enroll() throws Exception{
 		// 주어진 값
 		Member member = new Member();
-		member.setName("kim");
+		String name = "kim";
+		member.setName(name);
 
 		//trigger
-		Long saveId = service.join(member);
+		repo.save(member);
+		List<Member> mem = repo.findByName(name);
 
 
 		//match
-		Assertions.assertEquals(member, repo.findOne(saveId));
+		Assertions.assertEquals(1, mem.size());
 	}
 }
