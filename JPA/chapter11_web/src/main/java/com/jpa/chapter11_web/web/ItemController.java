3603cbadf76ad78597c4e38eee244ae1062a801b
newkayak12 <newkayak12@gmail.com>
2021-10-03 20:19:21 +0900

    chpater11_web

--- JPA/chapter11_web/src/main/java/com/jpa/chapter11_web/web/ItemController.java
+++ JPA/chapter11_web/src/main/java/com/jpa/chapter11_web/web/ItemController.java
@@ -36,6 +36,12 @@ public class ItemController {
 	@RequestMapping(value = "/itmes/{itemId}/edit", method = RequestMethod.POST)
 	public String updateItem(@ModelAttribute("item") Book item) {
 		service.saveItem(item);
+/*
+ * 	준영속 상태 수정하는 방법은 1. 변경감지 2. 병합 사용
+ * 
+ * 1.준영속 상태인 것을 select해서 영속으로 만들고 바꿔서 변경 감지를 사용하거나
+ * 2. em.merge()로 그 안의 모든 값을 변경하거나 
+ */
 		return "redirect:/items";
 	}
 	
